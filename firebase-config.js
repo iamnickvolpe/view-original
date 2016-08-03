d51c44721cb6b90b7c4f0895ee8090c0ae293e17
Nick Volpe <nvolpe@nvolpe-ltm.internal.salesforce.com>
2016-08-03 01:38:13 -0400

    Added firebase config

---
+++ firebase-config.js
@@ -0,0 +1,7 @@
+module.exports = { ENV_VARS: {
+    apiKey: process.env.FBC_APIKEY,
+    authDomain: process.env.FBC_AUTHDOMAIN,
+    databaseURL: process.env.FBC_DBURL,
+    storageBucket: process.env.FBC_STORAGE
+  }
+}
