5b80e0c14633c260e1952566294938254166370a
Nick Volpe <nvolpe@nvolpe-ltm.internal.salesforce.com>
2016-08-22 11:14:30 -0400

    Moved filter function out of watch.

--- src/components/subway/subway.directive.js
+++ src/components/subway/subway.directive.js
@@ -15,16 +15,19 @@ module.exports = function subway($http, $interval, $firebaseObject) {
               $scope.data = response.service.subway[0].line;
             });
           });
-        }  
+        }
+
+        $scope.lines = [];
+
+        $scope.filter = function(line) {
+          return line.status[0] !== "GOOD SERVICE" && $scope.lines[line.name[0]]===true;
+        }
 
         var subwayRef = firebase.database().ref('users/'+$scope.firebaseUser.uid+'/preferences/lines');
         var lines = $firebaseObject(subwayRef);
 
         lines.$watch(function() {
           $scope.lines = lines;
-          $scope.filter = function(line) {
-            return line.status[0] !== "GOOD SERVICE" && $scope.lines[line.name[0]]===true;
-          }
         });
 
       },
